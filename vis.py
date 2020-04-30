e780e4ec1f41ced10653166f78db18d686613359
Adam <casselldev@gmail.com>
2020-04-30 13:21:52 -0700

    Initial SummaryVis Commit. Implemented histogram.
    Co-authored-by: Nikkolas Irwin <nikkolas.james.irwin@gmail.com>
    Co-authored-by: Andrew Munoz <amunoz24@nevada.unr.edu>

--- vis.py
+++ vis.py
@@ -1,6 +1,39 @@
+# /usr/bin/env python3
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#
+#   Containerized Amazon Recommender System (CARS) Project
+#
+#   Authors: Brianna Blain-Castelli, Nikkolas Irwin, Adam Cassell, and Andrew Munoz
+#   Date: 04/01/2020
+#   Purpose: Build a Big Data application using a Conda environment and Docker.
+#   Course: CS 636 Big Data Systems
+#   Project: CARS is an application that builds a recommender system from datasets provided by
+#            UCSD (see citation below). 
+#
+#   Dataset URL: https://nijianmo.github.io/amazon/index.html
+#
+#   ***IMPORTANT*** You must download the dataset files for a particular category to your local machine yourself due
+#                   to their size. As long as your dataset files are in the same directory as the Dockerfile, then
+#                   they will be added to the volume and usable by the container as expected.
+#
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#
+#   Citation: Justifying recommendations using distantly-labeled reviews and fined-grained aspects
+#             Jianmo Ni, Jiacheng Li, Julian McAuley
+#             Empirical Methods in Natural Language Processing (EMNLP), 2019
+#             PDF: http://cseweb.ucsd.edu/~jmcauley/pdfs/emnlp19a.pdf
+#
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#
+#   Python Vis Module: Python module containing Vis class. To be used for all visualization types.
+#
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 import pandas
-from IPython.display import display
 import matplotlib.pyplot as plt
+from IPython.display import display
 
 class Vis:
 
@@ -13,7 +46,12 @@ class Vis:
             raise Exception("Invalid visualization type")
 
     def vis_summary(self, data):
+        # fig1, ax1 = plt.subplots()
+        # data.boxplot(ax=ax1)
+        # ax1.set_title('Ratings Summary')
         fig1, ax1 = plt.subplots()
-        data.boxplot(ax=ax1)
-        ax1.set_title('Ratings Summary')
+        num_bins = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5]
+        data.hist(ax=ax1,bins=num_bins, edgecolor='white')
+        ax1.set_title('Ratings Distribution')
+        ax1.grid(False)
         plt.show()
